d143e66aa29804064d82243e17fd5b801afa4422
Johan Larsson <johanlarsson@outlook.com>
2017-11-15 20:38:28 +0000

    version 3.0.1

--- inst/doc/introduction.R
+++ inst/doc/introduction.R
@@ -1,9 +1,4 @@
-### R code from vignette source 'introduction.Rnw'
-### Encoding: ISO8859-1
-
-###################################################
-### code chunk number 1: setup
-###################################################
+## ----setup, include = FALSE----------------------------------------------
 knitr::knit_hooks$set(crop = knitr::hook_pdfcrop)
 knitr::knit_hooks$set(document = function(x) {
   sub('\\usepackage[]{color}', '\\usepackage{xcolor}', x, fixed = TRUE)
@@ -19,20 +14,13 @@ knitr::opts_chunk$set(
   size = "small",
   fig.align = "center",
   dev = "pdf",
-  dev.args = list(family = "sans", pointsize = 8),
-  cache = TRUE,
-  fig.path = "figure/graphics-",
-  cache.path = "cache/graphics-",
-  autodep = TRUE
+  dev.args = list(family = "sans", pointsize = 8)
 )
 library(lattice)
 lattice.options(default.theme = list(fontsize = list(text = 8, points = 4)))
 
 
-
-###################################################
-### code chunk number 2: input
-###################################################
+## ----input---------------------------------------------------------------
 library(eulerr)
 options(digits = 4)
 
@@ -50,74 +38,49 @@ mat <- cbind(
 )
 fit2 <- euler(mat)
 
-
-###################################################
-### code chunk number 3: introduction.Rnw:259-260
-###################################################
+## ------------------------------------------------------------------------
 fit2
 
+## ----residual-plot, eval = FALSE-----------------------------------------
+#  # Cleveland dot plot of the residuals
+#  lattice::dotplot(resid(fit2), xlab = "",
+#                   panel = function(...) {
+#                     panel.abline(v = 0, lty = 2)
+#                     panel.dotplot(...)
+#                   })
 
-###################################################
-### code chunk number 4: residual-plot (eval = FALSE)
-###################################################
-## # Cleveland dot plot of the residuals
-## lattice::dotplot(resid(fit2), xlab = "",
-##                  panel = function(...) {
-##                    panel.abline(v = 0, lty = 2)
-##                    panel.dotplot(...)
-##                  })
+## ----residual-dev, ref.label='residual-plot', echo = FALSE, fig.width = 3.5----
+# Cleveland dot plot of the residuals
+lattice::dotplot(resid(fit2), xlab = "",
+                 panel = function(...) {
+                   panel.abline(v = 0, lty = 2)
+                   panel.dotplot(...)
+                 })
 
-
-###################################################
-### code chunk number 5: residual-dev
-###################################################
-
-
-
-###################################################
-### code chunk number 6: coefs
-###################################################
+## ----coefs---------------------------------------------------------------
 coef(fit2)
 
-
-###################################################
-### code chunk number 7: introduction.Rnw:312-317
-###################################################
+## ------------------------------------------------------------------------
 wilkinson2012 <-  c(A = 4, B = 6, C = 3, D = 2, E = 7, F = 3,
                     "A&B" = 2, "A&F" = 2, "B&C" = 2, "B&D" = 1,
                     "B&F" = 2, "C&D" = 1, "D&E" = 1, "E&F" = 1,
                     "A&B&F" = 1, "B&C&D" = 1)
 fit3 <- euler(wilkinson2012, shape = "ellipse")
 
-
-###################################################
-### code chunk number 8: wilkinson2012
-###################################################
+## ----wilkinson2012, echo = FALSE, fig.width = 3.4, fig.height = 2.5------
 plot(fit3)
 
+## ----eulerr-plot, eval = FALSE-------------------------------------------
+#  plot(fit2)
 
-###################################################
-### code chunk number 9: eulerr-plot (eval = FALSE)
-###################################################
-## plot(fit2)
+## ----eulerr-dev, ref.label = "eulerr-plot", echo = FALSE-----------------
+plot(fit2)
 
+## ----custom-plot, eval = FALSE-------------------------------------------
+#  # Remove fills, vary border type, and switch fontface.
+#  plot(fit2, fill = "transparent", lty = 1:3, fontface = 4)
 
-###################################################
-### code chunk number 10: eulerr-dev
-###################################################
-
-
-
-###################################################
-### code chunk number 11: custom-plot (eval = FALSE)
-###################################################
-## # Remove fills, vary border type, and switch fontface.
-## plot(fit2, fill = "transparent", lty = 1:3, fontface = 4)
-
-
-###################################################
-### code chunk number 12: custom-plot
-###################################################
-
-
+## ----custom-plot, ref.label = "custom-plot", echo = FALSE----------------
+# Remove fills, vary border type, and switch fontface.
+plot(fit2, fill = "transparent", lty = 1:3, fontface = 4)
 
--- inst/doc/under-the-hood.R
+++ inst/doc/under-the-hood.R
@@ -1,9 +1,4 @@
-### R code from vignette source 'under-the-hood.Rnw'
-### Encoding: ISO8859-1
-
-###################################################
-### code chunk number 1: setup
-###################################################
+## ----setup, include = FALSE----------------------------------------------
 knitr::knit_hooks$set(crop = knitr::hook_pdfcrop)
 #knitr::knit_theme$set("default")
 
@@ -16,57 +11,36 @@ knitr::opts_chunk$set(
   crop = TRUE,
   fig.align = "center",
   dev = "pdf",
-  dev.args = list(family = "sans", pointsize = 8),
-  cache = TRUE,
-  fig.path = "figure/graphics-",
-  cache.path = "cache/graphics-"
+  dev.args = list(family = "sans", pointsize = 8)
 )
 library(lattice)
 lattice.options(default.theme = list(fontsize = list(text = 8, points = 4)))
 
-
-###################################################
-### code chunk number 2: under-the-hood.Rnw:214-217
-###################################################
+## ------------------------------------------------------------------------
 matrix(sample(c(TRUE, FALSE), 12, replace = TRUE),
        ncol = 3,
        dimnames = list(NULL, c("A", "B", "C")))
 
-
-###################################################
-### code chunk number 3: under-the-hood.Rnw:222-227
-###################################################
+## ------------------------------------------------------------------------
 matrix(c(TRUE, FALSE, FALSE,
          TRUE, TRUE, FALSE,
          FALSE, FALSE, TRUE),
        ncol = 3,
        dimnames = list(NULL, c("A", "B", "C")))
 
-
-###################################################
-### code chunk number 4: under-the-hood.Rnw:232-233
-###################################################
+## ------------------------------------------------------------------------
 as.table(apply(Titanic, 2:4, sum))
 
-
-###################################################
-### code chunk number 5: under-the-hood.Rnw:237-240
-###################################################
+## ------------------------------------------------------------------------
 list(A = c("x", "xy", "xyz"),
      B = c("xy"),
      C = c("x", "xyz"))
 
-
-###################################################
-### code chunk number 6: under-the-hood.Rnw:256-258
-###################################################
+## ------------------------------------------------------------------------
 library(eulerr)
 eulerr:::bit_indexr(3)
 
-
-###################################################
-### code chunk number 7: under-the-hood.Rnw:287-298
-###################################################
+## ------------------------------------------------------------------------
 r1 <- 0.7 #radius of set 1
 r2 <- 0.9 #radius of set 2
 overlap <- 1 #area of overlap
@@ -79,63 +53,36 @@ stats::optimize(eulerr:::discdisc, #computes the squared loss
 
 # minimum is our required distance
 
-
-###################################################
-### code chunk number 8: under-the-hood.Rnw:523-528
-###################################################
+## ------------------------------------------------------------------------
 combo <- c("A" = 1, "B" = 1, "C" = 1,
            "A&B" = 0.5, "A&C" = 0.5, "C&B" = 0.5)
 
 fit1 <- euler(combo)
 fit1
 
-
-###################################################
-### code chunk number 9: circle-plot
-###################################################
+## ----circle-plot, echo = FALSE-------------------------------------------
 plot(fit1, counts = TRUE)
 
-
-###################################################
-### code chunk number 10: under-the-hood.Rnw:542-544
-###################################################
+## ------------------------------------------------------------------------
 fit2 <- euler(combo, shape = "ellipse")
 fit2
 
-
-###################################################
-### code chunk number 11: ellipse-plot
-###################################################
+## ----ellipse-plot, echo = FALSE------------------------------------------
 plot(fit2, counts = TRUE)
 
+## ----legend-plot, eval = FALSE-------------------------------------------
+#  plot(euler(c(A = 2, B = 3, "A&B" = 1)), auto.key = TRUE)
 
-###################################################
-### code chunk number 12: legend-plot (eval = FALSE)
-###################################################
-## plot(euler(c(A = 2, B = 3, "A&B" = 1)), auto.key = TRUE)
+## ----legend-dev, ref.label = "legend-plot", echo = FALSE-----------------
+plot(euler(c(A = 2, B = 3, "A&B" = 1)), auto.key = TRUE)
 
+## ----counts-plot, eval = FALSE-------------------------------------------
+#  plot(euler(c(A = 2, B = 3, "A&B" = 1)), counts = TRUE)
 
-###################################################
-### code chunk number 13: legend-dev
-###################################################
+## ----counts-dev, ref.label = "counts-plot", echo = FALSE-----------------
+plot(euler(c(A = 2, B = 3, "A&B" = 1)), counts = TRUE)
 
-
-
-###################################################
-### code chunk number 14: counts-plot (eval = FALSE)
-###################################################
-## plot(euler(c(A = 2, B = 3, "A&B" = 1)), counts = TRUE)
-
-
-###################################################
-### code chunk number 15: counts-dev
-###################################################
-
-
-
-###################################################
-### code chunk number 16: under-the-hood.Rnw:615-621
-###################################################
+## ------------------------------------------------------------------------
 n <- 500
 seqn <- seq(0, n, 1)
 theta <- seqn*pi*(3 - sqrt(5))
@@ -143,13 +90,9 @@ rad <- sqrt(seqn/n)
 x <- rad*cos(theta)
 y <- rad*sin(theta)
 
-
-###################################################
-### code chunk number 17: under-the-hood.Rnw:625-629
-###################################################
+## ----echo = FALSE--------------------------------------------------------
 xyplot(y ~ x, asp = 1, pch = 16, cex = 0.5, axes = FALSE, xlab = NULL,
        col = 1,
        ylab = NULL, scales = list(draw = FALSE),
        par.settings = list(axis.line = list(col = "transparent")))
-
 
