a368326c62895b9da66134c6aa66fa5714c86bf8
Johan Larsson <johanlarsson@outlook.com>
2024-02-16 13:10:06 +0000

    version 7.0.1

--- inst/doc/introduction.R
+++ inst/doc/introduction.R
@@ -10,9 +10,15 @@ options(digits = 4)
 library(eulerr)
 
 # Input in the form of a named numeric vector
-fit1 <- euler(c("A" = 25, "B" = 5, "C" = 5,
-                "A&B" = 5, "A&C" = 5, "B&C" = 3,
-                "A&B&C" = 3))
+fit1 <- euler(c(
+  "A" = 25,
+  "B" = 5,
+  "C" = 5,
+  "A&B" = 5,
+  "A&C" = 5,
+  "B&C" = 3,
+  "A&B&C" = 3
+))
 
 # Input as a matrix of logicals
 set.seed(1)
@@ -28,12 +34,7 @@ fit2
 
 ## ----residual-plot, fig.cap = "Residuals for the fit diagram."----------------
 # Cleveland dot plot of the residuals
-library(lattice)
-dotplot(resid(fit2), xlab = "",
-        panel = function(...) {
-          panel.abline(v = 0, lty = 2)
-          panel.dotplot(...)
-        })
+dotchart(resid(fit2))
 
 ## ----error-plot, fig.cap = "A plot from `error_plot()`."----------------------
 error_plot(fit2)
@@ -42,10 +43,24 @@ error_plot(fit2)
 coef(fit2)
 
 ## ----fig.cap = "A difficult combination from Wilkinson 2012."-----------------
-wilkinson2012 <-  c(A = 4, B = 6, C = 3, D = 2, E = 7, F = 3,
-                    "A&B" = 2, "A&F" = 2, "B&C" = 2, "B&D" = 1,
-                    "B&F" = 2, "C&D" = 1, "D&E" = 1, "E&F" = 1,
-                    "A&B&F" = 1, "B&C&D" = 1)
+wilkinson2012 <- c(
+  A = 4,
+  B = 6,
+  C = 3,
+  D = 2,
+  E = 7,
+  F = 3,
+  "A&B" = 2,
+  "A&F" = 2,
+  "B&C" = 2,
+  "B&D" = 1,
+  "B&F" = 2,
+  "C&D" = 1,
+  "D&E" = 1,
+  "E&F" = 1,
+  "A&B&F" = 1,
+  "B&C&D" = 1
+)
 fit3 <- euler(wilkinson2012, shape = "ellipse")
 plot(fit3)
 
@@ -53,9 +68,11 @@ plot(fit3)
 plot(fit2)
 
 # Remove fills, vary borders, display quantities, and switch font.
-plot(fit2,
-     quantities = TRUE,
-     fill = "transparent",
-     lty = 1:3,
-     labels = list(font = 4))
+plot(
+  fit2,
+  quantities = TRUE,
+  fill = "transparent",
+  lty = 1:3,
+  labels = list(font = 4)
+)
 
